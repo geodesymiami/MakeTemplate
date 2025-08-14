438157e1eacecf4b17efca8a5e929c2c4aa8792c
Falk Amelung <famelung@earth.miami.edu>
2025-08-14 22:03:13 +0200

    Use upstream version of read_excel.py

--- src/maketemplate/read_excel.py
+++ src/maketemplate/read_excel.py
@@ -1,11 +1,10 @@
 import os
-import sys
 import pandas as pd
 
 scratch = os.getenv('SCRATCHDIR')
 
 def main(file_name):
-    path = os.path.join(scratch, file_name)
+    path = os.path.join(scratch, file_name) if not(os.path.isabs(file_name)) else file_name
 
     if not os.path.exists(path):
         raise FileNotFoundError(f"File {file_name} does not exist in {scratch}")
